40f4432c8ab495953606d7a62f6b2c3e408fb1f7
Mateo GIRARDOT <girardot.mateo@gmail.com>
2021-08-25 17:13:13 +0200

    Save Commit

--- 42/autre/trial/main_c07/mainex04.c
+++ 42/autre/trial/main_c07/mainex04.c
@@ -6,13 +6,17 @@
 /*   By: mgirardo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/24 18:47:13 by mgirardo          #+#    #+#             */
-/*   Updated: 2021/08/25 16:26:41 by mgirardo         ###   ########.fr       */
+/*   Updated: 2021/08/25 17:12:56 by mgirardo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+
 char	*ft_convert_base(char *nbr, char *base_from, char *base_to);
 
-int	main(void)
+int	main(int ac, char **av)
 {
-	
+	(void) ac;
+	printf("%s\n", ft_convert_base(av[1], av[2], av[3]));
+	return (0);
 }
