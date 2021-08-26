84c5c3c3deec1417dc60bc92349154ccdb915f22
Mateo GIRARDOT <girardot.mateo@gmail.com>
2021-08-26 15:46:06 +0200

    Save Commit

--- 42/autre/trial/main_c07/mainex05.c
+++ 42/autre/trial/main_c07/mainex05.c
@@ -6,14 +6,34 @@
 /*   By: mgirardo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/25 19:37:58 by mgirardo          #+#    #+#             */
-/*   Updated: 2021/08/26 13:15:56 by mgirardo         ###   ########.fr       */
+/*   Updated: 2021/08/26 15:45:54 by mgirardo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
 char	**ft_split(char *str, char *charset);
 
-int	main(int ac, char **ac)
+int	main(int ac, char **av)
 {
+	char	**tab;
+	unsigned int	i;
+	unsigned int	j;
+
 	(void) ac;
-	printf()
+	(void) av;
+	tab = ft_split("en voila une belle maison toute rouge", " e");
+	i = 0;
+	while (tab[i])
+	{
+		j = 0;
+		while (tab[i][j])
+		{
+			write (1, &tab[i][j], 1);
+			j++;
+		}
+		write (1, "\n", 1);
+		i++;
+	}
+	return (0);
 }
--- 42/c07/rendu_c07/ex05/ft_split.c
+++ 42/c07/rendu_c07/ex05/ft_split.c
@@ -6,16 +6,18 @@
 /*   By: mgirardo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/25 19:40:43 by mgirardo          #+#    #+#             */
-/*   Updated: 2021/08/26 13:15:52 by mgirardo         ###   ########.fr       */
+/*   Updated: 2021/08/26 15:45:50 by mgirardo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdlib.h>
 
 int	chek_if_charset(char stri, char *charset)
 {
 	int	i;
 
 	i = 0;
-	if (charset[i] = '\0')
+	if (charset[i] == '\0')
 		return (-1);
 	while (charset[i])
 	{
@@ -35,7 +37,7 @@ int	get_first_word(char *str, char *charset)
 		return (0);
 	while (str[i] && chek_if_charset(str[i], charset) == 1)
 		i++;
-	return (i)
+	return (i);
 }
 
 int	ft_count_word(char *str, char *charset)
@@ -43,15 +45,15 @@ int	ft_count_word(char *str, char *charset)
 	int	nbword;
 	int	i;
 	int	j;
-	
+
 	i = get_first_word(str, charset) - 1;
 	nbword = 0;
 	if (chek_if_charset(str[i], charset) == -1
-			&& chek_if_charset(str[i], str) != -1)
+		&& chek_if_charset(str[i], str) != -1)
 		return (1);
 	while (str[++i])
 	{
-		if (chek_if_charset(str[i], charset))
+		if (chek_if_charset(str[i], charset) == 1)
 		{
 			while (str[i] && chek_if_charset(str[i], charset) == 1)
 				i++;
@@ -61,20 +63,40 @@ int	ft_count_word(char *str, char *charset)
 			while (str[j] && chek_if_charset(str[j], charset) != 1 )
 				j++;
 			if (str[j] == '\0')
-				return (++nbword)
+				return (++nbword);
 		}
 	}
 	return (nbword);
 }
 
-void	ft_remplissage(char *str, char *charset, int nbword)
+char	*pick_word(char *str, int start, int stop)
 {
+	int		range;
+	int		i;
+	char	*ptr;
+	int		j;
+
+	range = stop - start;
+	ptr = malloc(sizeof(*str) * (range + 1));
+	i = start;
+	j = 0;
+	while (i < stop)
+	{
+		ptr[j] = str[i];
+		i++;
+		j++;
+	}
+	ptr[j] = '\0';
+	return (ptr);
 }
 
 char	**ft_split(char *str, char *charset)
 {
 	int		nbword;
 	char	**tab;
+	int		i;
+	int		pword;
+	int		start;
 
 	nbword = ft_count_word(str, charset);
 	tab = malloc(sizeof(*tab) * (nbword + 1));
@@ -84,4 +106,17 @@ char	**ft_split(char *str, char *charset)
 	start = i;
 	if (nbword == 0 && str[i] && chek_if_charset(str[i], charset) != 1)
 		nbword++;
+	pword = 0;
+	while (str[i++])
+	{
+		if (chek_if_charset(str[i], charset) == 1 || str[i] == '\0')
+		{
+			tab[pword++] = pick_word(str, start, i);
+			while (chek_if_charset(str[i], charset) == 1)
+				i++;
+			start = i;
+		}
+	}
+	tab[nbword] = 0;
+	return (tab);
 }
