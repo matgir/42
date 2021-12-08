4df2bf731acfd2a480139e923d2ea9324e5eca79
Mateo GIRARDOT <mgirardo@e2r6p16.clusters.42paris.fr>
2021-12-08 18:02:56 +0100

    Save Commit

--- Libft/mainlibft/mainft_memchr.c
+++ Libft/mainlibft/mainft_memchr.c
@@ -1,25 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*   mainft_memchr.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mgirardo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/12/08 15:38:20 by mgirardo          #+#    #+#             */
-/*   Updated: 2021/12/08 18:01:55 by mgirardo         ###   ########.fr       */
+/*   Created: 2021/12/08 17:50:45 by mgirardo          #+#    #+#             */
+/*   Updated: 2021/12/08 17:52:54 by mgirardo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <stddef.h>
 
-void    *ft_memchr(const void *s, int o, size_t n)
+void	*ft_memchr(const void *s, int o, size_t n);
+
+int main()
 {
 	
-    while (n-- != 0)
-    {
-	
-        if (*((unsigned char *) s++) == (unsigned char) o)
-		return ((void *) --s);
-    }
+    unsigned char   s[] = "bonsoir voici une lettre";
+    int             o = 105;
+    size_t          n = 25;
+
+    printf("%p\n", ft_memchr(s, o, n));
+    printf("%p\n", memchr(s, o, n));
     return (0);
 }
