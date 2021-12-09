8fcbbaa4fca14f721adf0b9956e3a8ed22fbbce4
Mateo GIRARDOT <mgirardo@e2r10p14.clusters.42paris.fr>
2021-12-09 14:55:09 +0100

    Save Commit

--- Libft/mainlibft/mainft_strlcpy.c
+++ Libft/mainlibft/mainft_strlcpy.c
@@ -6,14 +6,15 @@
 /*   By: mgirardo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/09 14:19:53 by mgirardo          #+#    #+#             */
-/*   Updated: 2021/12/09 14:38:12 by mgirardo         ###   ########.fr       */
+/*   Updated: 2021/12/09 14:52:54 by mgirardo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 #include <stdio.h>
 #include <string.h>
-#include "libft.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 
 size_t	strlcpy(char * dst, const char * src, size_t maxlen)
 {
@@ -33,11 +34,19 @@ int	main(void)
 {
 	char	strsrc[] = "je suis un test il ce peut que je concatene";
 	char	strdest[] = "voila";
-	unsigned int	i;
+	unsigned int	i = 15;
 
-	i =	6;
 	printf("%s\n%s\n", strsrc, strdest);
 	printf("%ld\n", ft_strlcpy(strdest, strsrc, i));
 	printf("%s\n%s\n", strsrc, strdest);
+
+	char	strsrc2[] = "je suis un test il ce peut que je concatene";
+	char	strdest2[] = "voila";
+	unsigned int	i2 = 15;
+
+	printf("%s\n%s\n", strsrc2, strdest2);
+	printf("%ld\n", strlcpy(strdest2, strsrc2, i2));
+	printf("%s\n%s\n", strsrc2, strdest2);
+
 	return (0);
 }
--- Libft/rendu_libft/ft_strlcpy.c
+++ Libft/rendu_libft/ft_strlcpy.c
@@ -6,18 +6,18 @@
 /*   By: mgirardo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/23 14:32:49 by mgirardo          #+#    #+#             */
-/*   Updated: 2021/12/07 17:00:42 by mgirardo         ###   ########.fr       */
+/*   Updated: 2021/12/09 14:51:18 by mgirardo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stddef.h>
 #include "libft.h"
+#include <stddef.h>
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
 	size_t	i;
 
-	if (size > '0')
+	if (size > 0)
 	{
 		i = 0;
 		while (i < size - 1)
