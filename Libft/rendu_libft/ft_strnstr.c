935a52af673ed593fbafc2ba2a2f6981edd03baa
Mateo GIRARDOT <mgirardo@e2r10p14.clusters.42paris.fr>
2021-12-09 12:10:55 +0100

    Save Commit

--- Libft/rendu_libft/ft_strnstr.c
+++ Libft/rendu_libft/ft_strnstr.c
@@ -6,90 +6,97 @@
 /*   By: mgirardo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/08 15:36:57 by mgirardo          #+#    #+#             */
-/*   Updated: 2021/12/08 15:37:19 by mgirardo         ###   ########.fr       */
+/*   Updated: 2021/12/09 12:10:31 by mgirardo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-vim#include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+//#include "libft.h"
+#include <stddef.h>
 
-size_t  ft_strlen(char *to_find)
+size_t	ft_strlen(const char *s)
 {
-	
-    size_t  i;
+	size_t	i;
 
-    i = 0;
-    while ((to_find[i])
-        i++]));
-    return ((i)));
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
 }
 
-char    *verif ((char *str, char *to_find, size_t i, size_t j)
+char	*verif(const char *big, const char *little, size_t i, size_t len)
 {
-	
-    size_t  l;
+	size_t	l;
+	size_t	j;
 
-    while ( (str[i])
-    {
-	
-        //j = 0;
-        if ( (str[i] == to_find[j])
-        {
-	
-            l = i;
-            while ( (l <= (i + ft_strlen(to_find)) && (str[l] || to_find[j]))
-            {
-	
-                if ( (str[l++] == to_find[j++])
-                {
-	
-                    //l++;
-                    //j++;
-                    if ( (j == ft_strlen(to_find))
-                        return ( (&str[l - j])])))));
-                }
-                else
-                    break;
-            }
-        }
-        i++;
-    }
-    return ( (0)));
+	while (len-- != 0)
+	{
+		j = 0;
+		if (big[i] == little[j])
+		{
+			l = i;
+			while (l <= (i + ft_strlen(little)) && (big[l] || little[j]))
+			{
+				if (big[l++] == little[j++])
+				{
+					if (j == ft_strlen(little))
+						return ((char *)&big[l - j]);
+				}
+				else
+					break ;
+			}
+		}
+		i++;
+	}
+	return (0);
 }
 
-char    *ft_strstr(char *str, char *to_find)
+char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
-	
-    unsigned int    i;
-    unsigned int    j;
+	size_t	i;
 
-    i = -1;
-    j = 0;
-    if ( (ft_strlen(to_find) == 0)
-        return ( (str))))));
-    return ( (verif ((str, to_find, i, j))))));
+	i = 0;
+	if (ft_strlen(little) == 0)
+		return ((char *)big);
+	return (verif (big, little, i, len));
 }
 
-/*char    *strnstr(const char *big, const char *little, size_t len)
-{
-	
+/*#if defined(LIBC_SCCS) && !defined(lint)
+static char sccsid[] = "@(#)strstr.c	8.1 (Berkeley) 6/4/93";
+#endif 
+//LIBC_SCCS and not lint
+#include <sys/cdefs.h>
 
+char	*strnstr(const char *s, const char *find, size_t slen)
+{
+	char c, sc;
+	size_t len;
+
+	if ((c = *find++) != '\0')
+	{	
+		len = strlen(find);
+		do
+		{	
+			do
+			{	
+				if (slen-- < 1 || (sc = *s++) == '\0')
+					return (NULL);
+			}
+			while (sc != c);
+			if (len > slen)
+				return (NULL);
+		}
+		while (strncmp(s, find, len) != 0);
+		s--;
+	}
+	return ((char *)s);
 }*/
 
-int main()
+int	main(void)
 {
-	
-    printf("%s\n", strstr("bonsoir je suis moi", "bon"))"""")"");
-    printf("%s\n", ft_strstr("bonsoir je suis moi", "bon"))"""")"");
-    return ( 0);
+	//printf("%s\n", strnstr("bonsoir je suis moi", "e", 8));
+	printf("%s\n", ft_strnstr("bonsoir je suis moi", "je", 8));
+	return (0);
 }
-})
-})
-})
-}]]))
-}]])))))
-}]]))
-}]))
-}))
-})
