6ced893efc6cc7bfd02897a14245879596b49c8e
Girardot Mateo <girardot.mateo@gmail.com>
2021-12-10 17:30:59 +0100

    Save Commit

--- Libft/mainlibft/mainft_strrchr.c
+++ Libft/mainlibft/mainft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: mgirardo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/10 16:57:46 by mgirardo          #+#    #+#             */
-/*   Updated: 2021/12/10 16:58:09 by mgirardo         ###   ########.fr       */
+/*   Updated: 2021/12/10 17:25:01 by mgirardo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ char	*ft_strrchr(const char *s, int c);
 
 int	main(void)
 {
-	const char	s[] = "";
-	int		c = 116;
+	const char	s[] = "bonsoir bonsoir";
+	int		c = 115;
 
-	printf("%s\n", ft_strrchr(s, c));
 	printf("%s\n", strrchr(s, c));
+	printf("%s\n", ft_strrchr(s, c));
 }
--- Libft/rendu_libft/ft_strrchr.c
+++ Libft/rendu_libft/ft_strrchr.c
@@ -1,20 +1,62 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mgirardo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/12/10 16:27:47 by mgirardo          #+#    #+#             */
-/*   Updated: 2021/12/10 16:54:49 by mgirardo         ###   ########.fr       */
+/*   Created: 2021/12/10 16:59:11 by mgirardo          #+#    #+#             */
+/*   Updated: 2021/12/10 17:28:58 by mgirardo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strchr(const char *s, int c)
+/*
+char	*ft_strrchr(const char *s, int c)
 {
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == (const char)c)
+		{
+			j = i;
+			while (s[j])
+			{
+				if (s[j] == (const char)c)
+					break;
+				j++;
+			}
+		}
+		i++;
+	}
+	return (0);
+}
+*/
+
+#include "libft.h"
+
+char	*ft_strrchr(const char *s, int c)
+{
+	size_t	nb_c_in_s;
+	size_t	i;
+
+	nb_c_in_s = 0;
+	i = ft_strlen(s);
 	while (s)
 	{
 		if (*s == (const char)c)
+			nb_c_in_s++;
+		s++;
+	}
+	s -= i;
+	i = 0;
+	while (s)
+	{
+		if (*s == (const char)c)
+			i++;
+		if (*s == (const char)c && i == nb_c_in_s)
 			return ((char *)s);
 		s++;
 	}
