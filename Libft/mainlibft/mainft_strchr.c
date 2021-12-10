0251178cff1aa24ac1211c8b0a248063cd935282
Girardot Mateo <girardot.mateo@gmail.com>
2021-12-10 16:44:00 +0100

    Save Commit

---
+++ Libft/mainlibft/mainft_strchr.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mainft_strchr.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgirardo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/10 16:26:06 by mgirardo          #+#    #+#             */
+/*   Updated: 2021/12/10 16:41:45 by mgirardo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+#include <stdio.h>
+
+char	*ft_strchr(const char *s, int c);
+
+int	main(void)
+{
+	const char	s[] = "bonsoir";
+	int		c = 116;
+
+printf("%s\n", ft_strchr(s, c));
+printf("%s\n", strchr(s, c));
+}
