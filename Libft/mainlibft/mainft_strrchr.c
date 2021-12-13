75ba157ad0238a0468e8856add98e4e381dbd1fc
Girardot Mateo <girardot.mateo@gmail.com>
2021-12-13 11:38:51 +0100

    Save Commit

--- Libft/mainlibft/mainft_strrchr.c
+++ Libft/mainlibft/mainft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: mgirardo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/10 16:57:46 by mgirardo          #+#    #+#             */
-/*   Updated: 2021/12/10 17:25:01 by mgirardo         ###   ########.fr       */
+/*   Updated: 2021/12/13 11:38:02 by mgirardo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@ char	*ft_strrchr(const char *s, int c);
 int	main(void)
 {
 	const char	s[] = "bonsoir bonsoir";
-	int		c = 115;
+	int		c = 98;
 
-	printf("%s\n", strrchr(s, c));
 	printf("%s\n", ft_strrchr(s, c));
+	printf("%s\n", strrchr(s, c));
 }
--- Libft/rendu_libft/ft_strrchr.c
+++ Libft/rendu_libft/ft_strrchr.c
@@ -6,62 +6,11 @@
 /*   By: mgirardo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/10 16:59:11 by mgirardo          #+#    #+#             */
-/*   Updated: 2021/12/13 11:26:21 by mgirardo         ###   ########.fr       */
+/*   Updated: 2021/12/13 11:38:36 by mgirardo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*
-char	*ft_strrchr(const char *s, int c)
-{
-	size_t	i;
-	size_t	j;
-
-	i = 0;
-	while (s[i])
-	{
-		if (s[i] == (const char)c)
-		{
-			j = i;
-			while (s[j])
-			{
-				if (s[j] == (const char)c)
-					break;
-				j++;
-			}
-		}
-		i++;
-	}
-	return (0);
-}
-*/
-
 #include "libft.h"
-
-/*char	*ft_strrchr(const char *s, int c)
-{
-	size_t	nb_c_in_s;
-	size_t	i;
-
-	nb_c_in_s = 0;
-	i = ft_strlen(s);
-	while (s)
-	{
-		if (*s == (const char)c)
-			nb_c_in_s++;
-		s++;
-	}
-	s -= i;
-	i = 0;
-	while (s)
-	{
-		if (*s == (const char)c)
-			i++;
-		if (*s == (const char)c && i == nb_c_in_s)
-			return ((char *)s);
-		s++;
-	}
-	return (0);
-}*/
 
 char	*ft_strrchr(const char *s, int c)
 {
@@ -70,8 +19,8 @@ char	*ft_strrchr(const char *s, int c)
 	i = ft_strlen(s);
 	while (i != 0)
 	{
-		if ((*s + i) == (long unsigned int)c)
-			return ((char *)s);
+		if (s[i] == (const char)c)
+			return ((char *)s + i);
 		i--;
 	}
 	return (0);
