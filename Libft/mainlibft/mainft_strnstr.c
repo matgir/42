6c4230c3448be36ae08e9c06ae0bfd80982f6fe3
Girardot Mateo <girardot.mateo@gmail.com>
2021-12-15 14:38:13 +0100

    Save Commit

--- Libft/mainlibft/mainft_strnstr.c
+++ Libft/mainlibft/mainft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: mgirardo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/09 12:23:46 by mgirardo          #+#    #+#             */
-/*   Updated: 2021/12/15 12:05:19 by mgirardo         ###   ########.fr       */
+/*   Updated: 2021/12/15 12:17:18 by mgirardo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@ char	*strnstr(const char *s, const char *find, size_t slen)
 	char c, sc;
 	size_t len;
 
+	printf("%ld\n", slen);
 	if ((c = *find++) != '\0')
 	{	
 		len = strlen(find);
@@ -46,12 +47,21 @@ int	main(void)
 {
 	char s1[] = "lorem ipsum dolor sit amet";
 	char s2[] = "dolor";
-	size_t len = 23;
 	char haystack[30] = "aaabcabcd";
+	char needle[10] = "aabc";
+	//char *empty = (char *)"";
 
-	printf("%s\n", strnstr(s1, s2, len));
-	printf("%s\n", ft_strnstr(s1, s2, len));
+	printf("%s\n", strnstr(s1, s2, 20));
+	printf("%s\n", ft_strnstr(s1, s2, 20));
 	printf("%s\n", strnstr(haystack, "cd", 10));
 	printf("%s\n", ft_strnstr(haystack, "cd", 10));
+	printf("%s\n", strnstr(haystack, needle, -1));
+	printf("%s\n", ft_strnstr(haystack, needle, -1));
+	printf("%s\n", strnstr(haystack, "aaabc", 5));
+	printf("%s\n", ft_strnstr(haystack, "aaabc", 5));
+	printf("%s\n", strnstr(haystack, "abcd", 9));
+	printf("%s\n", ft_strnstr(haystack, "abcd", 9));
+	printf("%s\n", strnstr(haystack, "a", 1));
+	printf("%s\n", ft_strnstr(haystack, "a", 1));
 	return (0);
 }
--- Libft/rendu_libft/ft_strnstr.c
+++ Libft/rendu_libft/ft_strnstr.c
@@ -6,80 +6,36 @@
 /*   By: mgirardo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/08 15:36:57 by mgirardo          #+#    #+#             */
-/*   Updated: 2021/12/15 11:54:27 by mgirardo         ###   ########.fr       */
+/*   Updated: 2021/12/15 14:37:32 by mgirardo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
 #include <stddef.h>
-
-/*static char	*verif(const char *big, const char *little, size_t i, size_t len)
-  {
-  size_t	l;
-  size_t	j;
-  size_t	lenlittle;
-
-  lenlittle = ft_strlen(little);
-  while (i < len && big[i])
-  {
-  j = 0;
-  if (big[i] == little[j])
-  {
-  l = i;
-  while (l < (i + lenlittle) && (big[l] && little[j]) && l < len)
-  {
-  if (big[l] == little[j])
-  {
-  if (j == lenlittle - 1)
-  return ((char *)&big[l - j]);
-  l++;
-  j++;
-  }
-  else
-  break ;
-  }
-  }
-  i++;
-  }
-  return (0);
-  }
-
-  char	*ft_strnstr(const char *big, const char *little, size_t len)
-  {
-  size_t	i;
-
-  i = 0;
-  if (ft_strlen(little) == 0)
-  return ((char *)big);
-  return (verif (big, little, i, len));
-  }*/
-
-static char	*verif(const char *big, const char *little, size_t i, size_t len)
-{
-	size_t	j;
-	size_t	lenlittle;
-
-	lenlittle = ft_strlen(little);
-	while (i < len && big[i])
-	{
-		j = 0;
-		while (big[i] == little[j] && i < len)
-		{
-			if (j == lenlittle - 1)
-				return ((char *)&big[i - j]);
-			i++;
-			j++;
-		}
-	}
-	return (0);
-}
 
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
 	size_t	i;
+	size_t	l;
+	size_t	j;
 
 	i = 0;
-	if (ft_strlen(little) == 0)
+	if (little[0] == 0)
 		return ((char *)big);
-	return (verif (big, little, i, len));
+	while (i < len && big[i])
+	{
+		j = 0;
+		if (big[i] == little[j])
+		{
+			l = i;
+			while (big[l] && little[j] && l < len)
+			{
+				if (big[l] == little[j] && little[j + 1] == '\0')
+					return ((char *)&big[l - j]);
+				l++;
+				j++;
+			}
+		}
+		i++;
+	}
+	return (0);
 }
