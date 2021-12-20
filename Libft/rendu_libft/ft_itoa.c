daa1200c5894dc6178ad7f93fdc8abbb9e60c3d0
Girardot Mateo <girardot.mateo@gmail.com>
2021-12-20 12:20:00 +0100

    Save Commit

--- Libft/rendu_libft/ft_itoa.c
+++ Libft/rendu_libft/ft_itoa.c
@@ -6,35 +6,52 @@
 /*   By: mgirardo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/17 17:34:06 by mgirardo          #+#    #+#             */
-/*   Updated: 2021/12/20 11:26:22 by mgirardo         ###   ########.fr       */
+/*   Updated: 2021/12/20 12:19:15 by mgirardo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
-#include <stdio.h>
-#include "libft.h"
+
+static char	*ft_sign(long int ntmp, int i)
+{
+	char	*str;
+
+	if (ntmp < 0)
+	{
+		ntmp *= -1;
+		str = malloc(sizeof(char) * (++i + 1));
+		str[0] = '-';
+		str[i] = '\0';
+		while (--i > 0)
+		{
+			str[i] = ntmp % 10 + 48;
+			ntmp /= 10;
+		}
+	}
+	else
+	{
+		str = malloc(sizeof(char) * (i + 1));
+		str[i] = '\0';
+		while (--i >= 0)
+		{
+			str[i] = ntmp % 10 + 48;
+			ntmp /= 10;
+		}
+	}
+	return (str);
+}
 
 char	*ft_itoa(int n)
 {
 	long int	ntmp;
 	int			i;
-	char		*str;
 
 	i = 1;
 	ntmp = n;
-	while (n/10 != 0)
+	while (n / 10 != 0)
 	{	
-		n = n/10;
+		n = n / 10;
 		i++;
 	}
-	str = malloc(sizeof(char) * (i + 1));
-	str[i] = '\0';
-	while (--i >= 0)
-	{
-		str[i] = ntmp%10 + 48;
-		ntmp = ntmp/10;
-	}
-	if (n < 0)
-		str = ft_strjoin("-", str);
-	return (str);
+	return (ft_sign(ntmp, i));
 }
--- Libft/rendu_libft/libft.h
+++ Libft/rendu_libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: mgirardo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/07 16:10:17 by mgirardo          #+#    #+#             */
-/*   Updated: 2021/12/17 13:27:56 by mgirardo         ###   ########.fr       */
+/*   Updated: 2021/12/20 11:37:38 by mgirardo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,17 @@
 #ifndef LIBFT_H
 # define LIBFT_H
 
-int		ft_atoi(const char *nptr);
+int			ft_atoi(const char *nptr);
 void		ft_bzero(void *s, size_t n);
 void		*ft_calloc(size_t nmemb, size_t size);
-int		ft_isalnum(int c);
-int		ft_isalpha(int c);
-int		ft_isascii(int c);
-int		ft_isdigit(int c);
-int		ft_isprint(int c);
+int			ft_isalnum(int c);
+int			ft_isalpha(int c);
+int			ft_isascii(int c);
+int			ft_isdigit(int c);
+int			ft_isprint(int c);
+char		*ft_itoa(int n);
 void		*ft_memchr(const void *s, int o, size_t n);
-int		ft_memcmp(const void *s1, const void *s2, size_t n);
+int			ft_memcmp(const void *s1, const void *s2, size_t n);
 void		*ft_memcpy(void *dest, const void *src, size_t n);
 void		*ft_memmove(void *dest, const void *src, size_t n);
 void		*ft_memset(void *s, int c, size_t n);
@@ -35,12 +36,12 @@ char		*ft_strjoin(const char *s1, const char *s2);
 size_t		ft_strlcat(char *dst, const char *src, size_t size);
 size_t		ft_strlcpy(char *dst, const char *src, size_t size);
 size_t		ft_strlen(const char *s);
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int			ft_strncmp(const char *s1, const char *s2, size_t n);
 char		*ft_strnstr(const char *big, const char *little, size_t len);
 char		*ft_strrchr(const char *s, int c);
 char		*ft_strtrim(char const *s1, char const *set);
 char		*ft_substr(char const *s, unsigned int start, size_t len);
-int		ft_tolower(int c);
-int		ft_toupper(int c);
+int			ft_tolower(int c);
+int			ft_toupper(int c);
 
 #endif
