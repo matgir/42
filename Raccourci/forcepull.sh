8d42e1f63fc6f159fabb3576c26949bee1cb9675
Girardot Mateo <girardot.mateo@gmail.com>
2022-01-19 12:22:18 +0100

    Save Commit

--- Raccourci/forcepull.sh
+++ Raccourci/forcepull.sh
@@ -6,7 +6,7 @@
 #    By: mgirardo <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/01/07 12:12:45 by mgirardo          #+#    #+#              #
-#    Updated: 2022/01/18 10:59:09 by mgirardo         ###   ########.fr        #
+#    Updated: 2022/01/19 12:22:09 by mgirardo         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -22,6 +22,8 @@ cp -r ft_printf ~/Desktop/42
 cp .zshrc ~/.zshrc
 cp .vimrc ~/.vimrc
 cp -r Raccourci ~
+
+echo modifi save.sh et .zshrc selon OS
 
 # A chaque pullgit sur une machine differentes changer save.sh et .zshrc
 # en focntion de linux ou mac os
