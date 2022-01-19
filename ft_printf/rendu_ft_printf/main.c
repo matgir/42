fe2e17b8b9b3235e4de3f5f03455c581f28bac27
Girardot Mateo <girardot.mateo@gmail.com>
2022-01-19 16:19:09 +0100

    Save Commit

--- ft_printf/rendu_ft_printf/main.c
+++ ft_printf/rendu_ft_printf/main.c
@@ -6,7 +6,7 @@
 /*   By: mgirardo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/18 16:41:00 by mgirardo          #+#    #+#             */
-/*   Updated: 2022/01/19 16:05:27 by mgirardo         ###   ########.fr       */
+/*   Updated: 2022/01/19 16:18:49 by mgirardo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	main(void)
 {
 	char	c = ' ';
 	char	str[] = "et bonjour";
-	int		i = -51474;
+	int		i = 2147483647;
 
 	printf("\n%i\n", ft_printf("%%bonsoir%c%s %i", c, str, i));
 	printf("\n%i\n", printf("%%bonsoir%c%s %i", c, str, i));
