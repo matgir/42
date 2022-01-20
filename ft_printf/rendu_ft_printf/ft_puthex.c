69435f9daccdf6b133fc1fc62e1425f5ad882f2f
Mateo GIRARDOT <mgirardo@e1r8p18.42.fr>
2022-01-20 17:00:22 +0100

    Save Commit

--- ft_printf/rendu_ft_printf/ft_puthex.c
+++ ft_printf/rendu_ft_printf/ft_puthex.c
@@ -6,7 +6,7 @@
 /*   By: mgirardo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/20 11:22:06 by mgirardo          #+#    #+#             */
-/*   Updated: 2022/01/20 16:52:24 by mgirardo         ###   ########.fr       */
+/*   Updated: 2022/01/20 16:55:59 by mgirardo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ int	ft_puthex(unsigned int n, char *hexbase)
 	unsigned int	ntmp;
 	int				i;
 	char			*str;
+	int				len;
 
 	i = 1;
 	ntmp = n;
@@ -25,6 +26,7 @@ int	ft_puthex(unsigned int n, char *hexbase)
 		n = n / 16;
 		i++;
 	}
+	len = i;
 	str = malloc(sizeof(char) * (i + 1));
 	if (str == NULL)
 		return (0);
@@ -35,5 +37,6 @@ int	ft_puthex(unsigned int n, char *hexbase)
 		ntmp /= 16;
 	}
 	ft_putstr(str);
-	return (ft_strlen(str));
+	free(str);
+	return (len);
 }
