462c5f950142d0e59aa490290bb13d3e706655a3
Girardot Mateo <girardot.mateo@gmail.com>
2022-03-29 18:48:44 +0200

    Save Commit

--- 42_cursus/FdF/rendu_FdF/minilibx-linux/maintest.c
+++ 42_cursus/FdF/rendu_FdF/minilibx-linux/maintest.c
@@ -9,6 +9,10 @@
 /*   Updated: 2022/03/24 11:09:53 by mgirardo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+
+
+
 /*#include <stdlib.h>
 #include <stdio.h>
 
@@ -21,7 +25,7 @@
 
 #define MLX_ERROR 1
 
-#define RED_PIXEL 0x0000FF
+#define RED_PIXEL 0xFF0000
 
 typedef struct s_data
 {
@@ -43,8 +47,7 @@ int	render(t_data *data)
 {
 //	if window has been destroyed, we don't want to put the pixel !
 	if (data->win_ptr != NULL)
-		mlx_pixel_put(data->mlx_ptr, data->win_ptr, 
-			WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2, RED_PIXEL);
+		mlx_pixel_put(data->mlx_ptr, data->win_ptr, WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2, RED_PIXEL);
 	return (0);
 }
 
@@ -55,8 +58,7 @@ int	main(void)
 	data.mlx_ptr = mlx_init();
 	if (data.mlx_ptr == NULL)
 		return (MLX_ERROR);
-	data.win_ptr = mlx_new_window(data.mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT,
-								"my window");
+	data.win_ptr = mlx_new_window(data.mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT, "my window");
 	if (data.win_ptr == NULL)
 	{
 		free(data.win_ptr);
@@ -73,6 +75,12 @@ int	main(void)
 	mlx_destroy_display(data.mlx_ptr);
 	free(data.mlx_ptr);
 }*/
+
+
+
+
+
+
 
 /*#include <X11/X.h>
 #include <X11/keysym.h>
@@ -103,7 +111,12 @@ int	main(void)
 	mlx_loop(data.mlx);
 }*/
 
-#include <stdlib.h>
+
+
+
+
+
+/*#include <stdlib.h>
 #include "mlx.h"
 
 int	main(void)
@@ -113,4 +126,143 @@ int	main(void)
 	mlx = mlx_init();
 	mlx_destroy_display(mlx);
 	free(mlx);
+}*/
+
+
+
+
+
+
+
+
+
+/*#include "mlx.h"
+#include <unistd.h>
+#include <stdlib.h>
+
+typedef struct s_data
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+	int		color;
+	int		x;
+	int		y;
+}	t_data;
+
+void	ft_putchar_fd(char c, int fd)
+{
+	write(fd, &c, 1);
+}
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	long int	nbr;
+
+	nbr = n;
+	if (nbr < 0)
+	{
+		nbr *= -1;
+		ft_putchar_fd('-', fd);
+	}
+	if (nbr >= 10)
+		ft_putnbr_fd(nbr / 10, fd);
+	ft_putchar_fd(nbr % 10 + 48, fd);
+}
+
+int	deal_key(int key, t_data *data)
+{
+	if (key == 65307)
+	{
+		mlx_destroy_window(data->mlx_ptr, data->win_ptr);
+		data->win_ptr = NULL;
+	}
+	return(0);
+}
+
+int	main()
+{
+	t_data	data;
+
+	data.mlx_ptr = mlx_init();
+	data.win_ptr = mlx_new_window(data.mlx_ptr, 500, 500, "Bonsoir");
+	mlx_pixel_put(data.mlx_ptr, data.win_ptr, 250, 250, 0x00FFFFFF);
+	mlx_key_hook(data.win_ptr, deal_key, &data);
+	mlx_loop(data.mlx_ptr);
+	mlx_destroy_display(data.mlx_ptr);
+	free(data.mlx_ptr);
+}*/
+
+/*#include "mlx.h"
+#include <stddef.h>
+
+typedef struct	s_data
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+}				t_data;
+
+typedef struct	p_data
+{
+	int	start_point_x;
+	int	start_point_y;
+}				p_data;
+
+int	main()
+{
+	int		x = 10;
+	int		y = 10;
+	t_data	data;
+	p_data	start;
+	int		win_height;
+	int		win_width;
+	int		go_down;
+	int		go_right;
+	int		tmp_x;
+
+	data.mlx_ptr = mlx_init();
+	if (data.mlx_ptr == NULL)
+		return(1);
+	win_height = 1000;
+	win_width = 1000;
+	data.win_ptr = mlx_new_window(data.mlx_ptr, win_width, win_height, "Test");
+	if (data.win_ptr == NULL)
+		return(1);
+	start.start_point_x = 50;
+	start.start_point_y = 50;
+	tmp_x = start.start_point_x;
+	go_down = 0;
+	while(go_down <= y)
+	{
+		go_right = 0;
+		while(go_right <= x)
+		{
+			mlx_pixel_put(data.mlx_ptr, data.win_ptr, start.start_point_x, start.start_point_y, 0xFFFFFF);
+			start.start_point_x += 10;
+			go_right++;
+		}
+		start.start_point_x = tmp_x;
+		start.start_point_y += 10;
+		go_down++;
+	}
+	mlx_loop(data.mlx_ptr);
+	return(0);
+}
+//	permet d'afficher un rectangle de point de longueur x et hauteur y
+*/
+
+
+//	objectif creer un un tableau de tableau avec dans chque case une struct ou un str de coordonnees du point
+//	pour une representation comme voulu dans le rendu final
+
+typedef struct	c_data
+{
+	int	x_axe;
+	int	y_axe;
+}				x_n_y;
+
+#include "mlx.h"
+
+int	main()
+{
+	x_n_y
 }
