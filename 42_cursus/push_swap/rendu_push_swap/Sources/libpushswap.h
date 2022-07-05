ad340e4ed0b93b7f419aadd95489f3710b4c5ffa
Mateo GIRARDOT <mgirardo@e1r12p10.clusters.42paris.fr>
2022-07-05 16:43:49 +0200

    Save Commit

--- 42_cursus/push_swap/rendu_push_swap/Sources/libpushswap.h
+++ 42_cursus/push_swap/rendu_push_swap/Sources/libpushswap.h
@@ -20,10 +20,19 @@
 # include <stdlib.h>
 # include "../libft/libft.h"
 
-int	main(int ac, char **av);
-int	ft_ps_parsing(int ac, char **av);
-int	ft_ps_atoi(char *nptr, int *error);
-int	ft_ps_intcheck(char **av);
-int	ft_ps_duplicatecheck(char **av);
+typedef struct	v_list
+{
+	int					content;
+	struct v_list		*next;
+}				t_ps_list;
+
+int			main(int ac, char **av);
+int			ft_ps_parsing(int ac, char **av);
+int			ft_ps_atoi(char *nptr, int *error);
+int			ft_ps_intcheck(char **av);
+int			ft_ps_duplicatecheck(char **av);
+t_ps_list	*fill_stack_a(char **av);
+t_ps_list	*ft_ps_lstnew(int content);
+void		ft_ps_lstadd_back(t_ps_list **alst, t_ps_list *new);
 
 #endif
--- 42_cursus/push_swap/rendu_push_swap/Sources/main.c
+++ 42_cursus/push_swap/rendu_push_swap/Sources/main.c
@@ -12,9 +12,63 @@
 
 #include "libpushswap.h"
 
+void	ft_ps_lstadd_back(t_ps_list **alst, t_ps_list *new)
+{
+	t_ps_list	*ptr;
+
+	if (*alst == NULL)
+		*alst = new;
+	else
+	{
+		ptr = *alst;
+		while (ptr->next != NULL)
+			ptr = ptr->next;
+		ptr->next = new;
+	}
+}
+
+t_ps_list	*ft_ps_lstnew(int content)
+{
+	t_ps_list	*new;
+
+	new = malloc(sizeof(t_ps_list));
+	if (new == NULL)
+		return (NULL);
+	new->content = content;
+	new->next = NULL;
+	return (new);
+}
+
+t_ps_list	*fill_stack_a(char **av)
+{
+	t_ps_list	*stack_a;
+	t_ps_list	*tmp_lst;
+	int		i;
+	int		tmp;
+
+	i = 0;
+	stack_a = NULL;
+	while (av[++i])
+	{
+		tmp = atoi(av[i]);
+		tmp_lst = ft_ps_lstnew(tmp);
+		if (tmp_lst == NULL)
+		{
+			ft_putendl_fd("Error alloc", 2);
+			return (NULL); // maybe do a function that free everything
+		}
+		ft_ps_lstadd_back(&stack_a, tmp_lst);
+	}
+	return (stack_a);
+}
+
 int	main(int ac, char **av)
 {
+	t_ps_list	*stack_a;
+	
 	if (ft_ps_parsing(ac, av) == 0)
 		return (0);
-	ft_printf("ready for the real shit");
+	stack_a = fill_stack_a(av);
+	if (stack_a == NULL)
+		return (0);
 }
