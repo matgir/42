3d3e27206ea24073e5fdf3ff6861d07beaad54ea
Mateo GIRARDOT <mgirardo@e1r12p10.42.fr>
2022-07-05 12:23:41 +0200

    Save Commit

--- 42_cursus/push_swap/rendu_push_swap/Sources/libpushswap.h
+++ 42_cursus/push_swap/rendu_push_swap/Sources/libpushswap.h
@@ -1,10 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libpushswap.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgirardo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/05 11:25:53 by mgirardo          #+#    #+#             */
+/*   Updated: 2022/07/05 11:26:09 by mgirardo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stddef.h>
-#include<string.h>
-#include<stdio.h>
-#include<stddef.h>
-#include<stdlib.h>
-# include "../libft/libft.h"
+#include <string.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include "../libft/libft.h"
 
 int	main(int ac, char** av);
 int	ft_ps_parsing(int ac, char** av);
 int	ft_ps_atoi(char *nptr, int *error);
+int	ft_ps_intcheck(char **av);
+int	ft_ps_duplicatecheck(char **av);
--- 42_cursus/push_swap/rendu_push_swap/Sources/main.c
+++ 42_cursus/push_swap/rendu_push_swap/Sources/main.c
@@ -1,30 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgirardo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/05 11:26:46 by mgirardo          #+#    #+#             */
+/*   Updated: 2022/07/05 11:26:58 by mgirardo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libpushswap.h"
 
-int	ft_ps_parsing(int ac, char** av)
+int	ft_ps_intcheck(char **av)
 {
-	int i;
-	int j;
-	int error;
+	int	i;
+	int	error;
 
-	if (ac < 3)
-		return (1);
-	i = -1;
+	i = 0;
 	while (av[++i])
 	{
 		error = 0;
 		ft_ps_atoi(av[i], &error);
 		if (error != 1)
 		{
-			ft_putendl_fd("Error 1", 2);
+			ft_putendl_fd("Error not int", 2);
 			return(0);
 		}
-		j = i - 1;
+	}
+	return (1);
+}
+
+int	ft_ps_duplicatecheck(char **av)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (av[++i])
+	{	
+		j = i;
 		while (av[++j])
 		{
-			if (ft_ps_atoi(av[i], &error) == ft_ps_atoi(av[j], &error) && error == 1)
+			if (ft_atoi(av[i]) == ft_atoi(av[j]))
 			{
-
-				ft_putendl_fd("Error 2", 2);
+				ft_putendl_fd("Error duplicate", 2);
 				return(0);
 			}
 		}
@@ -32,7 +52,21 @@ int	ft_ps_parsing(int ac, char** av)
 	return (1);
 }
 
-int	main(int ac, char** av)
+int	ft_ps_parsing(int ac, char **av)
+{
+	if (ac < 2)
+	{
+		ft_putendl_fd("Error param", 2);
+		return (0);
+	}
+	if (ft_ps_intcheck(av) == 0)
+		return (0);
+	if (ft_ps_duplicatecheck(av) == 0)
+		return (0);
+	return (1);
+}
+
+int	main(int ac, char **av)
 {
 	if (ft_ps_parsing(ac, av) == 0)
 		return (0);
