18a07312320259d8370d1e382105af0fcf3a7cc7
Mateo GIRARDOT <mgirardo@e1r7p1.clusters.42paris.fr>
2022-09-20 17:54:14 +0200

    Save Commit

--- 42_cursus/Philo/rendu_philo/Sources/test_philo.c
+++ 42_cursus/Philo/rendu_philo/Sources/test_philo.c
@@ -9,43 +9,100 @@
 /*   Updated: 2022/09/19 14:32:34 by mgirardo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-/*
+
 #include <stdio.h>
 #include <pthread.h>
 
-# define NC		"\e[0m"
-# define YELLOW	"\e[1;33m"
+# define NC			"\e[0m"
+# define BYELLOW	"\e[1;33m"
+# define YELLOW		"\e[33m"
+# define RED		"\e[31m"
+# define GREEN		"\e[32m"
 
-void	*thread_routine(void *data)
+# define TIMES_TO_COUNT 21000
+
+typedef struct s_counter
 {
-	pthread_t	t_id;
+	pthread_mutex_t	count_mutex;
+	unsigned int	count;
+}					t_counter;
 
-	(void)data;
-	t_id = pthread_self();
-//	pthread_self() is forbidden in philo project but used here to know the
-//	thread's ID
-	printf("%sThread [%ld] : Hello World !%s\n", YELLOW, t_id, NC);
+void	*thread_routine1(void *data)
+{
+	t_counter	*counter;
+	unsigned int	i;
+
+	counter = (t_counter *)data;
+	pthread_mutex_lock(&counter->count_mutex);
+	printf("%sThread [1] : Count start %u!%s\n", YELLOW, counter->count, NC);
+	pthread_mutex_unlock(&counter->count_mutex);
+	i = -1;
+	while (++i < TIMES_TO_COUNT)
+	{
+		pthread_mutex_lock(&counter->count_mutex);
+		counter->count++;
+		pthread_mutex_unlock(&counter->count_mutex);
+	}
+	pthread_mutex_lock(&counter->count_mutex);
+	printf("%sThread [1] : Final count %u!%s\n", YELLOW, counter->count, NC);
+	pthread_mutex_unlock(&counter->count_mutex);
+	return (NULL);
+}
+
+void	*thread_routine2(void *data)
+{
+	t_counter	*counter;
+	unsigned int	i;
+
+	counter = (t_counter *)data;
+	pthread_mutex_lock(&counter->count_mutex);
+	printf("%sThread [2] : Count start %u!%s\n", YELLOW, counter->count, NC);
+	pthread_mutex_unlock(&counter->count_mutex);
+	i = -1;
+	while (++i < TIMES_TO_COUNT)
+	{
+		pthread_mutex_lock(&counter->count_mutex);
+		counter->count++;
+		pthread_mutex_unlock(&counter->count_mutex);
+	}
+	pthread_mutex_lock(&counter->count_mutex);
+	printf("%sThread [2] : Final count %u!%s\n", YELLOW, counter->count, NC);
+	pthread_mutex_unlock(&counter->count_mutex);
 	return (NULL);
 }
 
 int	main(void)
 {
-	pthread_t	t_id_1;
-	pthread_t	t_id_2;
+	pthread_t		t_id_1;
+	pthread_t		t_id_2;
+	t_counter		counter;
 
-	pthread_create(&t_id_1, NULL, thread_routine, NULL);
-	printf("1st thread created [%ld]\n", t_id_1);
-	pthread_create(&t_id_2, NULL, thread_routine, NULL);
-	printf("2nd thread created [%ld]\n", t_id_2);
+	counter.count = 0;
+
+	pthread_mutex_init(&counter.count_mutex, NULL);
+
+	printf("Main : Expected count = %s%u%s\n", GREEN, 2 * TIMES_TO_COUNT, NC);
+
+	pthread_create(&t_id_1, NULL, thread_routine1, &counter);
+	printf("Main : 1st thread created [%ld]\n", t_id_1);
+	pthread_create(&t_id_2, NULL, thread_routine2, &counter);
+	printf("Main : 2nd thread created [%ld]\n", t_id_2);
+
 	pthread_join(t_id_1, NULL);
-	printf("Joining 1st thread [%ld]\n", t_id_1);
+	printf("Main : Joining 1st thread [%ld]\n", t_id_1);
 	pthread_join(t_id_2, NULL);
-	printf("Joining 2nd thread [%ld]\n", t_id_2);
+	printf("Main : Joining 2nd thread [%ld]\n", t_id_2);
+
+	if (counter.count != (2 * TIMES_TO_COUNT))
+		printf("%sMain : ERROR, total count = %u !%s", RED, counter.count, NC);
+	else
+		printf("%sMain : GOOD, total count = %u !%s\n", GREEN, counter.count, NC);
+	pthread_mutex_destroy(&counter.count_mutex);
 	return (0);
 }
-*/
 
-#include <stdio.h>
+
+/* #include <stdio.h>
 #include <pthread.h>
 
 # define NC	"\e[0m"
@@ -87,4 +144,4 @@ int	main(void)
 	pthread_join(tid2, NULL);
 	printf("Main: Joining second thread [%ld]\n", tid2);
 	return (0);
-}
+} */
