b6e679093e366e69afb30744f8c4d8ab6965b456
Mateo GIRARDOT <mgirardo@e1r2p3.clusters.42paris.fr>
2022-09-21 17:08:09 +0200

    Save Commit

--- 42_cursus/Philo/rendu_philo/Sources/philo.c
+++ 42_cursus/Philo/rendu_philo/Sources/philo.c
@@ -1,43 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libphilo.h                                         :+:      :+:    :+:   */
+/*   philo.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mgirardo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/21 13:04:25 by mgirardo          #+#    #+#             */
-/*   Updated: 2022/09/21 13:04:30 by mgirardo         ###   ########.fr       */
+/*   Created: 2022/09/21 16:34:36 by mgirardo          #+#    #+#             */
+/*   Updated: 2022/09/21 16:34:50 by mgirardo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBPHILO_H
-# define LIBPHILO_H
+#include <pthread.h>
 
-/* ########## STRUCT ########## */
+typedef struct s_omni
+{
+	pthread_mutex_t	is_dead_mutex;
+	unsigned int	is_dead;
+	unsigned int	times_eaten;
+}					t_omni;
 
-typedef struct philo
+typedef struct s_philo
 {
 	int	nb_of_time_eaten; /* maybe to compare with the required amount each
 	they eat, or not because not always needeed to check, do it if needeed
 	with the omniscent thread */
 	int	dead; /* set to 0 at the beginning and to 1 if philo is dead */
-	int	time_to_die;
-	int	time_to_eat;
+	int	die_ms;
+	int	eat_ms;
+	int	sleep_ms;
 	int	holding_forks;
-};
+	int	id;
+}		t_philo;
 
-/* ########## USEFULL ########## */
+int	main(int ac, char **av)
+{
 
-# define NC			"\e[0m"
-# define BYELLOW	"\e[1;33m"
-# define YELLOW		"\e[33m"
-# define RED		"\e[31m"
-# define BLACK		"\e[30m"
-# define BLUE		"\e[34m"
-# define PURPLE		"\e[35m"
-# define CYAN		"\e[36m"
-# define ICYAN		"\e[0;96m"
-# define GREEN		"\e[32m"
-# define UGREEN		"\e[4;32m"
-
-#endif
+}
