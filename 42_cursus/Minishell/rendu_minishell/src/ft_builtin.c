ad8b8d64f712fa05d10113ed31eb02251ca2e658
Mateo GIRARDOT <mgirardo@e1r11p19.clusters.42paris.fr>
2022-10-17 21:38:28 +0200

    Save Commit

--- 42_cursus/Minishell/rendu_minishell/src/ft_builtin.c
+++ 42_cursus/Minishell/rendu_minishell/src/ft_builtin.c
@@ -15,13 +15,23 @@
 int	ft_nonewline(char *str)
 {
 	unsigned int	i;
+	unsigned int	j;
+	unsigned int	z;
 
 	i = 0;
+	j = 0;
+	z = 0;
 	if (str[i] == '-')
+	{
+		j = 1;
 		i++;
+	}
 	while (str[i] == 'n')
+	{
+		z = 1;
 		i++;
-	if (str[i] == '\0')
+	}
+	if (str[i] == '\0' && z == 1 && j == 1)
 		return (0);
 	else
 		return (1);
@@ -42,36 +52,44 @@ void	ft_echo(t_command *command)
 {
 	unsigned int	i;
 
-	i = 1;
-	if (command->cmd[i][0] == '-' && ft_nonewline(command->cmd[i]))
+	i = 0;
+	if (command->cmd[i + 1])
 	{
-		ft_fillofdout(command, 1);
-		write(command->ofdout, "\n", 1);
+		i = 1;
+		if (ft_nonewline(command->cmd[i]))
+		{
+			ft_fillofdout(command, 1);
+			write(command->ofdout, "\n", 1);
+		}
+		else
+		{
+			while (command->cmd[i] && !ft_nonewline(command->cmd[i]))
+				i++;
+			ft_fillofdout(command, i);
+		}
 	}
 	else
-	{
-		while (!ft_nonewline(command->cmd[i]))
-			i++;
-		ft_fillofdout(command, i);
-	}
+		write(command->ofdout, "\n", 1);
 }
 
+
+
 /* void	ft_builtin(t_minishell *minishell, t_command *command)
-{
-	if (!ft_strcmp(command->cmd[0], "echo"))
-		ft_echo(command);
-	else if (!ft_strcmp(command->cmd[0], "cd"))
-		ft_cd(minishell, command);
-	else if (!ft_strcmp(command->cmd[0], "pwd"))
-		ft_pwd(minishell, command);
-	else if (!ft_strcmp(command->cmd[0], "export"))
-		ft_export(minishell, command);	
-	else if (!ft_strcmp(command->cmd[0], "unset"))
-		ft_unset(minishell, command);
-	else if (!ft_strcmp(command->cmd[0], "env"))
-		ft_env(minishell, command);
-	else if (!ft_strcmp(command->cmd[0], "exit"))
-		ft_exit(minishell, command);
-	else
-		ft_execute(minishell, command); //verif si bonne synthax
-} */
+   {
+   if (!ft_strcmp(command->cmd[0], "echo"))
+   ft_echo(command);
+   else if (!ft_strcmp(command->cmd[0], "cd"))
+   ft_cd(minishell, command);
+   else if (!ft_strcmp(command->cmd[0], "pwd"))
+   ft_pwd(minishell, command);
+   else if (!ft_strcmp(command->cmd[0], "export"))
+   ft_export(minishell, command);	
+   else if (!ft_strcmp(command->cmd[0], "unset"))
+   ft_unset(minishell, command);
+   else if (!ft_strcmp(command->cmd[0], "env"))
+   ft_env(minishell, command);
+   else if (!ft_strcmp(command->cmd[0], "exit"))
+   ft_exit(minishell, command);
+   else
+   ft_execute(minishell, command); //verif si bonne synthax
+   } */
