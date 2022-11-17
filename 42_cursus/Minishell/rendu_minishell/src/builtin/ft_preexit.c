000738b4ddf35690ac13050a5ef909d513fca0f5
Mateo GIRARDOT <mgirardo@e3r2p9.clusters.42paris.fr>
2022-11-17 16:39:19 +0100

    Save Commit

--- 42_cursus/Minishell/rendu_minishell/src/builtin/ft_preexit.c
+++ 42_cursus/Minishell/rendu_minishell/src/builtin/ft_preexit.c
@@ -6,7 +6,7 @@
 /*   By: mgirardo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 14:33:53 by mgirardo          #+#    #+#             */
-/*   Updated: 2022/11/07 14:33:54 by mgirardo         ###   ########.fr       */
+/*   Updated: 2022/11/16 15:35:52 by audreyer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,14 @@
 
 void	ft_preexit(t_minishell *minishell, t_command *command)
 {
-	printf("exit\n");
-	ft_exit(minishell, command->cmd[1]);
+//	printf("exit\n");
+/* 
+	The exit status shall be n, if specified, except that the behavior is
+	unspecified if n is not an unsigned decimal integer or is greater than 255.
+	Otherwise, the value shall be the exit value of the last command executed, or
+	zero if no command was executed. When exit is executed in a trap action, the
+	last command is considered to be the command that executed immediately
+	preceding the trap action.
+ */
+	ft_exit(minishell, command->cmd[0]);
 }
