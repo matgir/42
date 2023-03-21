a5dbd621f7df16172e92eec8a41fab41b100a3b8
Mateo GIRARDOT <mgirardo@e2r8p17.clusters.42paris.fr>
2023-03-21 12:29:56 +0100

    Save Commit

--- 42_cursus/CPP/cpp_00/exos_videos/main_namespace.cpp
+++ 42_cursus/CPP/cpp_00/exos_videos/main_namespace.cpp
@@ -31,5 +31,23 @@ namespace	Muf = Bar;
 
 int	main(void)
 {
+	printf("gl_var : [%d]\n", gl_var);
+	printf("f() : [%d]\n\n", f());
 
+	printf("Foo::gl_var : [%d]\n", Foo::gl_var);
+	printf("Foo::f() : [%d]\n\n", Foo::f());
+
+	printf("Bar::gl_var : [%d]\n", Bar::gl_var);
+	printf("Bar::f() : [%d]\n\n", Bar::f());
+
+	printf("Muf::gl_var : [%d]\n", Muf::gl_var);
+	printf("Muf::f() : [%d]\n\n", Muf::f());
+
+	printf("::gl_var : [%d]\n", ::gl_var);
+	printf("::f() : [%d]\n\n", ::f());
+
+	/*	premiere et derniere ligne execute la meme chose, derniere utiliser
+		lorsau'il y a plusieurs namespace*/
+
+	return 0;
 }
