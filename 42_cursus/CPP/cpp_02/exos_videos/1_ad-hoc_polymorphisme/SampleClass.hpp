1831e2e09a27169e157266d69b001d988a6c79f0
Mateo GIRARDOT <mgirardo@bess-f3r1s2.clusters.42paris.fr>
2023-10-05 18:17:36 +0200

    Save Commit

---
+++ 42_cursus/CPP/cpp_02/exos_videos/1_ad-hoc_polymorphisme/SampleClass.hpp
@@ -0,0 +1,17 @@
+#ifndef SAMPLECLASS_HPP
+# define SAMPLECLASS_HPP
+
+class	Sample
+{
+	public:
+
+		Sample(void);
+		~Sample(void);
+
+		void	bar(char const c) const;
+		void	bar(int const n) const;
+		void	bar(float const z) const;
+		void	bar(Sample const & i) const;
+};
+
+#endif
