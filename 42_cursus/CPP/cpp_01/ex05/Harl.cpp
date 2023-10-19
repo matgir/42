6585f06c0fdb78fff9829b04e3b611d156a50735
Mateo GIRARDOT <mgirardo@bess-f4r1s13.clusters.42paris.fr>
2023-10-19 17:47:13 +0200

    Save Commit

---
+++ 42_cursus/CPP/cpp_01/ex05/Harl.cpp
@@ -0,0 +1,35 @@
+#include "Harl.hpp"
+
+Harl::Harl()
+{
+	return;
+}
+
+Harl::~Harl()
+{
+	return;
+}
+
+void	Harl::debug(void)
+{
+	std::cout << "Here is some contextual information, use it to establish your diagnosis." << std::endl;
+	return;
+}
+
+void	Harl::info(void)
+{
+	std::cout << "Here is some detailled informations, you can now follow the execution of your programme." << std::endl;
+	return;
+}
+
+void	Harl::warning(void)
+{
+	std::cout << "I'm warning you, it is still manageable but i'm warning you !" << std::endl;
+	return;
+}
+
+void	Harl::error(void)
+{
+	std::cout << "I was not even able to warn you, it is now KAPUT so dif in and fix it !" << std::endl;
+	return;
+}
