8ebb87b8e89c024ca6547c710b189fc162220e67
Mateo GIRARDOT <mgirardo@made-f0Cr8s4.clusters.42paris.fr>
2023-11-28 16:16:56 +0100

    Save Commit

--- 42_cursus/CPP/cpp_01/rendu_cpp01/ex01/zombieHorde.cpp
+++ 42_cursus/CPP/cpp_01/rendu_cpp01/ex01/zombieHorde.cpp
@@ -2,6 +2,11 @@
 
 Zombie*	zombieHorde(int N, std::string name)
 {
+	if (N < 0)
+	{
+		std::cout << "The Horde cannot have a negative number of Zombies" << std::endl;
+		exit(1);
+	}
 	Zombie*	thehorde = new Zombie[N];
 	for (int i = 0; i < N; i++)
 	{
