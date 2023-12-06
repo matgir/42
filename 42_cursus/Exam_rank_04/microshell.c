8aad0888dd07ac9ea7156aacfd7ec39728bbdc07
Mateo GIRARDOT <mgirardo@made-f0Dr6s10.clusters.42paris.fr>
2023-12-06 12:53:42 +0100

    Save Commit

--- 42_cursus/Exam_rank_04/microshell.c
+++ 42_cursus/Exam_rank_04/microshell.c
@@ -1,94 +1,107 @@
+#include <stdio.h>
 #include <unistd.h>
+// #include <sys/types.h>
+#include <linux/limits.h>
+// #include <limits.h>
 #include <string.h>
 #include <sys/wait.h>
 
-void ft_print_err(char *str)
+void	ft_print_error(char *str)
 {
-	int i = 0;
+	int	i = 0;
 
 	while (str[i])
 		i++;
 	write (2, str, i);
 }
 
-int ft_execute(char **argv, int i, int temp_fd, char **env)
+int	ft_execute(char **av, int i, int tmp_fd, char **env)
 {
-	argv[i] = NULL;
-	close(temp_fd);
-	execve(argv[0], argv, env);
-	ft_print_err("error : cannot execute ");
-	ft_print_err(argv[0]);
-	ft_print_err("\n");
+	av[i] = NULL;
+	close(tmp_fd);
+	execve(av[0], av, env);
+	ft_print_error("ME error: cannot execute ");
+	// ft_print_error("error: cannot execute ");
+	ft_print_error(av[0]);
+	ft_print_error("\n");
 	return (1);
 }
 
-int main(int argc, char **argv, char **env)
+int	main(int ac, char **av, char **env)
 {
-	(void)argc;
-	int fd[2];
-	int i = 0;
-	int pid = 0;
-	int temp_fd = dup(STDIN_FILENO);
+	(void)ac;
+	int	fd[2];
+	int	i = 0;
+	int	pid = 0;
+	int	tmp_fd = dup(STDIN_FILENO);
 
-	while (argv[i] && argv[i + 1])
+	while (av[i] && av[i + 1])
 	{
-		argv = &argv[i + 1];
-		i = 0;
-		while (argv[i] && strcmp(argv[i], ";") && strcmp(argv[i], "|"))
+		av = &av[i + 1];
+		// i = 0;
+		while (av[i] && strcmp(av[i], ";") && strcmp(av[i], "|"))
 			i++;
-		// cd errors
-		if (strcmp(argv[0], "cd") == 0)
+		/*********** CD ERRORS ***********/
+		if (strcmp(av[0], "cd") == 0)
 		{
 			if (i != 2)
-				ft_print_err("error: cd: bad arguments\n");
-			else if (chdir(argv[1]) != 0)
+				ft_print_error("ME error: cd: bad arguments\n");
+				// ft_print_error("error: cd: bad arguments\n");
+			else if (chdir(av[1]) != 0)
 			{
-				ft_print_err("error : cd: cannot change directory to ");
-				ft_print_err(argv[1]);
-				ft_print_err("\n");
+				ft_print_error("ME error: cd: cannot change directory to ");
+				// ft_print_error("error: cd: cannot change directory to ");
+				ft_print_error(av[1]);
+				ft_print_error("\n");
+			}
+			{//
+				char	cwd[PATH_MAX];//
+				getcwd(cwd, PATH_MAX);//
+				ft_print_error(cwd);//
+				ft_print_error("\n");//
 			}
 		}
-		// ";" implementation
-		else if (argv != &argv[i] && (argv[i] == NULL || strcmp(argv[i], ";") == 0))
+		/*********** ";" IMPLEMENTATION ***********/
+		else if (av != &av[i] && (av[i] == NULL || strcmp(av[i], ";") == 0))
 		{
 			pid = fork();
 			if (pid == 0)
 			{
-				dup2(temp_fd, STDIN_FILENO);
-				if (ft_execute(argv, i, temp_fd, env))
-					return (1);
+				dup2(tmp_fd, STDIN_FILENO);
+				if (ft_execute(av, i, tmp_fd, env))
+					return(1);
 			}
 			else
 			{
-				close(temp_fd);
+				close(tmp_fd);
 				waitpid(-1, NULL, WUNTRACED);
-				temp_fd = dup(STDIN_FILENO);
+				tmp_fd = dup(STDIN_FILENO);
 			}
 		}
-		// "|" implementation
-		else if (argv != &argv[i] && strcmp(argv[i], "|") == 0)
+		/*********** "|" IMPLEMENTATION ***********/
+		else if (av != &av[i] && strcmp(av[i], "|") == 0)
 		{
 			pipe(fd);
 			pid = fork();
 			if (pid == 0)
 			{
-				dup2(temp_fd, STDIN_FILENO);
+				dup2(tmp_fd, STDIN_FILENO);
 				dup2(fd[1], STDOUT_FILENO);
 				close(fd[1]);
-				close(temp_fd);
-				if (ft_execute(argv, i, temp_fd, env))
+				close(tmp_fd);
+				if (ft_execute(av, i, tmp_fd, env))
 					return (1);
 			}
 			else
 			{
-				close(temp_fd);
+				close(tmp_fd);
 				close(fd[1]);
 				waitpid(-1, NULL, WUNTRACED);
-				temp_fd = dup(fd[0]);
+				tmp_fd = dup(fd[0]);
 				close(fd[0]);
 			}
 		}
 	}
-	close(temp_fd);
+	close(tmp_fd);
 	return (0);
 }
