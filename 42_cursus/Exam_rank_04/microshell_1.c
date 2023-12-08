057a5d55b90461d147a0ee7cd3a0090baed75aac
Mateo GIRARDOT <mgirardo@made-f0Dr6s10.clusters.42paris.fr>
2023-12-08 13:41:42 +0100

    Save Commit

--- 42_cursus/Exam_rank_04/microshell_1.c
+++ 42_cursus/Exam_rank_04/microshell_1.c
@@ -5,7 +5,7 @@
 int	error(char *error)
 {
 	while (*error)
-		write (2, error++, 1);
+		write(2, error++, 1);
 	return 1;
 }
 
@@ -18,7 +18,7 @@ int	cd(char **argv, int i)
 	return 0;
 }
 
-int	execution(char **argv, char **env, int i)
+int	execute(char **argv, char **env, int i)
 {
 	int	fd[2];
 	int	status;
@@ -26,19 +26,19 @@ int	execution(char **argv, char **env, int i)
 
 	if (has_pipe && pipe(fd) == -1)
 		return error("error: fatal\n");
-	
+
 	int	pid = fork();
 	if (!pid)
 	{
 		argv[i] = 0;
 		if (has_pipe && (dup2(fd[1], STDOUT_FILENO) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
-			return error("error: fatal");
+			return error("error: fatal\n");
 		execve(*argv, argv, env);
-		return ("error: fatal\n");
+		return error("error: cannot execute "), error(*argv), error("\n");
 	}
 	waitpid(pid, &status, 0);
-	if (has_pipe && (dup2(fd[0], STDOUT_FILENO) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
-		return error("error:fatal\n");
+	if (has_pipe && (dup2(fd[0], STDIN_FILENO) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
+		return error("error: fatal\n");
 	return WIFEXITED(status) && WEXITSTATUS(status);
 }
 
@@ -56,9 +56,9 @@ int	main(int argc, char **argv, char **env)
 			while (argv[i] && strcmp(argv[i], "|") && strcmp(argv[i], ";"))
 				i++;
 			if (!strcmp(*argv, "cd"))
-				status += cd(argv, i);
+				status = cd(argv, i);
 			else if (i)
-				status += execution(argv, env, i);
+				status = execute(argv, env, i);
 		}
 	}
 	return status;
