c33ec8909725eb2b726a66960bc236756aef7711
Mateo GIRARDOT <mgirardo@paul-f5Ar2s1.clusters.42paris.fr>
2024-03-06 15:31:13 +0100

    Save Commit

--- 42_cursus/CPP/cpp_06/ex00/AScalarConverter.hpp
+++ 42_cursus/CPP/cpp_06/ex00/AScalarConverter.hpp
@@ -6,19 +6,52 @@
 # define ASCALARCONVERTER_HPP
 
 # include <iostream>
+# include <cstdlib>
+# include <string>
+# include <cmath>
+// # include <cfloat>
 
 class AScalarConverter
 {
 	public:
 		virtual		~AScalarConverter(void) = 0;
-		static void	convert(void * info);
+		static void	convert(std::string input);
 };
 
 #endif
 
 #include AScalarConverter.hpp
 
-void	AScalarConverter::convert(void * info)
+bool	displayable(char c)
 {
-	
+	if (c < 32 || c > 126)
+		return false;
+	else
+		return true;
+}
+
+void	AScalarConverter::convert(std::string input)
+{
+	char	c;
+	int		i;
+	float	f;
+	double	d;
+
+	while (1)
+	{
+	if (input.length() == 1 && displayable((input.c_str())[0]))
+	{
+		c = reinterpret_cast<char>(input.c_str());
+		i = static_cast<int>(c);
+		f = static_cast<float>(c);
+		d = static_cast<double>(c);
+		break;
+	}
+	esle if ()
+	}
+
+	std::cout << "char: " << c << std::endl;
+	std::cout << "int: " << i << std::endl;
+	std::cout << "float: " << f << std::endl;
+	std::cout << "double: " << d << std::endl;
 }
