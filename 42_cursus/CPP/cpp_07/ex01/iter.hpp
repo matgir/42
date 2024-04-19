392200b97e7ae4addd468fb8bcf756b35964b463
Mateo GIRARDOT <mgirardo@bess-f2r1s4.clusters.42paris.fr>
2024-04-19 16:35:35 +0200

    Save Commit

---
+++ 42_cursus/CPP/cpp_07/ex01/iter.hpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+template<typename T>
+void	iter(T * array, size_t size, void (*f)(T &))
+{
+	for (int i = 0; i < size; i++)
+	{
+		f(array[i]);
+	}
+	return;
+}
+
+template<typename T>
+void	showMe(T & toPrint)
+{
+	std::cout << toPrint << std::cout << toPrint << std::endl;
+	return;
+}
---
+++ 42_cursus/CPP/cpp_07/ex01/main.c
@@ -0,0 +1,10 @@
+#include "iter.hpp"
+
+int	main(void)
+{
+	std::string	array[10];
+
+	for (int i = 0; i < 10, i++)
+		array[i] = 'bonjour' + i;
+	return 0;
+}
