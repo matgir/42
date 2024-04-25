c1e1b146c10d4ce43b50275090113b2b55f35848
Mateo GIRARDOT <mgirardo@bess-f2r1s4.clusters.42paris.fr>
2024-04-25 14:31:44 +0200

    Save Commit

--- 42_cursus/CPP/cpp_06/ex00/AScalarConverter.cpp
+++ 42_cursus/CPP/cpp_06/ex00/AScalarConverter.cpp
@@ -72,24 +72,16 @@ bool	asciiRange(char c)
 
 void	output(char c, int i, float f, double d)
 {
-	// std::cout << "In Output" << std::endl;//
-	// std::cout << f << std::endl; //
-	// if (asciiRange(c)/*  || f == INFINITY || f == -INFINITY */)
-	if (c == -1)
-	{
+	if (asciiRange(c))
 		std::cout << "char: impossible" << std::endl;
-		std::cout << "int: impossible" << std::endl;
-	}
 	else if (displayable(c))
-	{
 		std::cout << "char: '" << c << "'" << std::endl;
-		std::cout << "int: " << i << std::endl;
-	}
 	else
-	{
 		std::cout << "char: Non Displayable" << std::endl;
+	if (c == -12)
+		std::cout << "int: impossible" << std::endl;
+	else
 		std::cout << "int: " << i << std::endl;
-	}
 	std::cout << "float: " << f << (f == static_cast<float>(i) ? ".0f" : "f") << std::endl;
 	std::cout << "double: " << d << (d == static_cast<double>(i) ? ".0" : "") << std::endl;
 }
@@ -98,37 +90,37 @@ bool	pseudoLiterals(std::string input)
 {
 	if (input == "nan")
 	{
-		output(static_cast<char>(-1), static_cast<int>(-1),
+		output(static_cast<char>(-12), static_cast<int>(-1),
 				static_cast<float>(NAN), static_cast<double>(NAN));
 		return true;
 	}
 	else if (input == "nanf")
 	{
-		output(static_cast<char>(-1), static_cast<int>(-1),
+		output(static_cast<char>(-12), static_cast<int>(-1),
 				static_cast<float>(NAN), static_cast<double>(NAN));
 		return true;
 	}
 	else if (input == "inf")
 	{
-		output(static_cast<char>(-1), static_cast<int>(-1),
+		output(static_cast<char>(-12), static_cast<int>(-1),
 				static_cast<float>(INFINITY), static_cast<double>(INFINITY));
 		return true;
 	}
 	else if (input == "inff")
 	{
-		output(static_cast<char>(-1), static_cast<int>(-1),
+		output(static_cast<char>(-12), static_cast<int>(-1),
 				static_cast<float>(INFINITY), static_cast<double>(INFINITY));
 		return true;
 	}
 	else if (input == "-inf")
 	{
-		output(static_cast<char>(-1), static_cast<int>(-1),
+		output(static_cast<char>(-12), static_cast<int>(-1),
 				static_cast<float>(-INFINITY), static_cast<double>(-INFINITY));
 		return true;
 	}
 	else if (input == "-inff")
 	{
-		output(static_cast<char>(-1), static_cast<int>(-1),
+		output(static_cast<char>(-12), static_cast<int>(-1),
 				static_cast<float>(-INFINITY), static_cast<double>(-INFINITY));
 		return true;
 	}
@@ -153,25 +145,22 @@ void	AScalarConverter::convert(std::string input)
 			i = static_cast<int>(c);
 			f = static_cast<float>(c);
 			d = static_cast<double>(c);
-			std::cout << "CHAR" << std::endl; //
 			output(c, i, f, d);
 			break;
 		}
 		/*	#### float ####	*/
 		else if ((f_ocurence(input) || (f_ocurence(input) && dot_ocurence(input))) && neg(input) && is_num(input))
 		{
-			// long int n = atol(input.c_str());
-			// if ( n > FLT_MAX || n < -FLT_MAX)
-			//...
+			double	overflow = std::atof(input.c_str());
+			if (overflow > FLT_MAX || overflow < FLT_MIN)
+			{
+				std::cout << "Float overflow, could not convert" << std::endl;
+				break;
+			}
 			f = static_cast<float>(std::atof(input.c_str()));
-			// f = INFINITY; //
-			// f = NAN;//
 			d = static_cast<double>(f);
 			i = static_cast<int>(f);
 			c = static_cast<char>(i);
-			std::cout << "char: '" << c << "'" << std::endl;
-			std::cout << "FLOAT" << std::endl; //
-			std::cout << f << std::endl; //
 			output(c, i, f, d);
 			break;
 		}
@@ -181,17 +170,10 @@ void	AScalarConverter::convert(std::string input)
 			// long int n = atol(input.c_str());
 			// if ( n > DBL_MAX || n < -DBL_MAX)
 			//...
-
-
 			d = std::atof(input.c_str());
-			// d = -INFINITY; //
-			// d = NAN;//
-			// if (d == NAN)//
-			// break;//
 			f = static_cast<float>(d);
 			i = static_cast<int>(d);
 			c = static_cast<char>(i);
-			std::cout << "DOUBLE" << std::endl; //
 			output(c, i, f, d);
 			break;
 		}
@@ -199,25 +181,23 @@ void	AScalarConverter::convert(std::string input)
 		else if (neg(input) && is_num(input))
 		{
 
-			// long int n = atol(input.c_str());
-			// if ( n > INT_MAX || n < INT_MIN)
-			// std::cout << " overflow " << std::endl;
-			// else
-			// {
+			long int n = atol(input.c_str());
+			if ( n > 2147483647 || n < -2147483648)
+			{
+				std::cout << "Int overflow, could not convert" << std::endl;
+				break;
+			}
 			i = atoi(input.c_str());
 			c = static_cast<char>(i);
 			f = static_cast<float>(i);
 			d = static_cast<double>(i);
-			std::cout << "INT" << std::endl; //
 			output(c, i, f, d);
-			// }
 			break;
 		}
-		// else
-		// {
-		// 	std::cout << "char: impossible\nint: impossible" << std::endl;
-		// 	std::cout << "float: nanf\ndouble: nan" << std::endl;
-		// 	break;
-		// }
+		else
+		{
+			std::cout << "Could not convert to anything, try another input !" << std::endl;
+			break;
+		}
 	}
 }
--- 42_cursus/CPP/cpp_06/ex00/main.cpp
+++ 42_cursus/CPP/cpp_06/ex00/main.cpp
@@ -2,8 +2,6 @@
 
 int	main(int ac, char **av)
 {
-	std::cout << "Minimum value for float: " << FLT_MIN << std::endl;
-	std::cout << "Maximum value for float: " << FLT_MAX << std::endl;
 	if (ac != 2)
 	{
 		std::cout << "wrong number of arguments" << std::endl;
@@ -12,8 +10,4 @@ int	main(int ac, char **av)
 	else
 		AScalarConverter::convert(av[1]);
 	return 0;
-/* 	(void)ac;
-	(void)av;
-	AScalarConverter::convert(INFINITY);
- */	return 0;
 }
