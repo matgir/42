a3d094bff28a93e62d93d4bb1ac28b2c2df71497
Mateo GIRARDOT <mgirardo@bess-f2r2s11.clusters.42paris.fr>
2024-05-07 17:12:48 +0200

    Save Commit

---
+++ 42_cursus/CPP/cpp_08/ex02/MutantClass.hpp
@@ -0,0 +1,116 @@
+// replace canonical by your class name
+// replace getvariable(void)const par nouveau getteur
+// replace variable by your variable name
+
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+# include <iostream>
+# include <stack>
+
+template<typename T>
+class MutantStack
+{
+	public:
+
+		MutantStack< T >(void)
+		{
+			return;
+		}
+		MutantStack< T >(MutantStack const & copy)
+		{
+			this->_stack = this->getStack();
+		}
+		virtual ~MutantStack< T >(void)
+		{
+			return;
+		}
+
+		MutantStack	&operator=(MutantStack const & assign)
+		{
+			if (this != &assign)
+				this->_stack = assign.getStack();
+			return *this;
+		}
+
+		std::stack<T>	getStack(void)const
+		{
+			return this->_stack;
+		}
+
+		bool			empty(void)const
+		{
+			return this->_stack.empty();
+		}
+
+		unsigned int	size(void)const
+		{
+			return this->_stack.size();
+		}
+
+		T				top(void)
+		{
+			return this->_stack.top();
+		}
+
+		void			push(T & val)
+		{
+			this->_stack.push(val);
+			return;
+		}
+
+		void			emplace(T & args)
+		{
+			this->_stack.emplace(args);
+			return;
+		}
+
+		void			pop(void)
+		{
+			this->_stack.pop();
+			return;
+		}
+
+		void			swap(std::stack & x)
+		{
+			this->_stack.swap(x);
+			return;
+		}
+
+	private:
+
+		std::stack<T>	_stack;
+};
+
+#endif
+
+// #include "MutantStack.hpp"
+
+// MutantStack::MutantStack(void) : _variable(0)
+// {
+// 	std::cout << "MutantStack default constructor called" << std::endl;
+// 	return;
+// }
+
+// MutantStack::MutantStack(MutantStack const & copy)
+// {
+// 	std::cout << "MutantStack copy constructor called" << std::endl;
+// 	this->_variable = copy.getvariable();
+// 	return;
+// }
+
+// MutantStack::~MutantStack(void)
+// {
+// 	std::cout << "MutantStack destructor called" << std::endl;
+// 	return;
+// }
+
+// MutantStack	&MutantStack::operator=(MutantStack const & assign)
+// {
+// 	std::cout << "MutantStack assignment operator called" << std::endl;
+
+// 	if (this != &assign)
+// 		this->_variable = assign.getvariable();
+
+// 	return *this;
+// }
