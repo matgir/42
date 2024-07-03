3e3c8d79bf130af47e3aba84e682c0e2740ca1db
Mateo GIRARDOT <mgirardo@made-f0Dr8s3.clusters.42paris.fr>
2024-07-03 18:24:41 +0200

    Save Commit

---
+++ 42_cursus/Inception/rendu_inception/srcs/requirements/mariadb/tools/script_mysql.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+service mysql start;
+	#	starts mysql
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${DB_DATABASE}\`;"
+	#	creates a table if it does not exists with ${DB_DATABEASE} name indicated in .env
+mysql -e "CREATE USER IF NOT EXISTS \`${DB_USER}\`@'localhost' IDENTIFIED BY '${DB_PASSWORD}';"
+	#	creates user if not existing already with specified password from .env
+mysql -e "GRANT ALL PRIVILEGES ON \`${DB_DATABASE}\`.* TO \`${DB_USER}\`@'%' IDENTIFIED BY '{DB_PASSWORD}';"
+	#	grants all privileges on database to the created user
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';"
+	#	here changes the root password to ${DB_ROOT_PASSWORD} from .env
+mysql -e "FLUSH PRIVILEGES"
+	#	update the last changes in mysql
+mysqladmin -u root -p$DB_ROOT_PASSWORD shutdown
+	#	reboot mysql
+exec mysql_safe
+	#	command that mysql ask at start
