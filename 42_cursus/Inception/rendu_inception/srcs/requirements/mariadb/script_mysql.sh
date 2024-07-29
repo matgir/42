fe10047df8979df9f20dad76f69bb18b5bed7ac1
Mateo GIRARDOT <mgirardo@BESS-f2r4s1.clusters.42paris.fr>
2024-07-29 18:41:41 +0200

    Save Commit

--- 42_cursus/Inception/rendu_inception/srcs/requirements/mariadb/script_mysql.sh
+++ 42_cursus/Inception/rendu_inception/srcs/requirements/mariadb/script_mysql.sh
@@ -1,20 +1,22 @@
 
-# service mysql start
-# 	#	starts mysql
-# mysql -e "CREATE DATABASE IF NOT EXISTS \`${DB_DATABASE}\`;"
-# 	#	creates a table if it does not exists with ${DB_DATABEASE} name indicated in .env
-# mysql -e "CREATE USER IF NOT EXISTS \`${DB_USER}\`@'localhost' IDENTIFIED BY '${DB_PASSWORD}';"
-# 	#	creates user if not existing already with specified password from .env
-# mysql -e "GRANT ALL PRIVILEGES ON \`${DB_DATABASE}\`.* TO \`${DB_USER}\`@'%' IDENTIFIED BY '{DB_PASSWORD}';"
-# 	#	grants all privileges on database to the created user
-# mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';"
-# 	#	here changes the root password to ${DB_ROOT_PASSWORD} from .env
-# mysql -e "FLUSH PRIVILEGES"
-# 	#	update the last changes in mysql
-# mysqladmin -u root -p$DB_ROOT_PASSWORD shutdown
-# 	#	reboot mysql
-# exec mysqld_safe
-# 	#	command that mysql ask at start
+service mysql start
+	#	starts mysql
+sleep 5
+	# allow mysql to start
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${DB_DATABASE}\`;"
+	#	creates a table if it does not exists with ${DB_DATABEASE} name indicated in .env
+mysql -e "CREATE USER IF NOT EXISTS \`${DB_USER}\`@'localhost' IDENTIFIED BY '${DB_PASSWORD}';"
+	#	creates user if not existing already with specified password from .env
+mysql -e "GRANT ALL PRIVILEGES ON \`${DB_DATABASE}\`.* TO \`${DB_USER}\`@'%' IDENTIFIED BY '{DB_PASSWORD}';"
+	#	grants all privileges on database to the created user
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';"
+	#	here changes the root password to ${DB_ROOT_PASSWORD} from .env
+mysql -e "FLUSH PRIVILEGES"
+	#	update the last changes in mysql
+mysqladmin -u root -p$DB_ROOT_PASSWORD shutdown
+	#	reboot mysql
+exec mysqld_safe
+	#	command that mysql ask at start
 
 
 # service mysql start;
@@ -26,20 +28,20 @@
 # mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
 # exec mysqld_safe
 
-#!/bin/sh
-service mysql start 
+# #!/bin/sh
+# service mysql start 
 
-# CREATE USER #
-echo "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';" | mysql
+# # CREATE USER #
+# echo "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';" | mysql
 
-# PRIVILGES FOR ROOT AND USER FOR ALL IP ADRESS #
-echo "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';" | mysql
-echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$DB_ROOT_PASSWORD';" | mysql
-echo "FLUSH PRIVILEGES;" | mysql
+# # PRIVILGES FOR ROOT AND USER FOR ALL IP ADRESS #
+# echo "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';" | mysql
+# echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$DB_ROOT_PASSWORD';" | mysql
+# echo "FLUSH PRIVILEGES;" | mysql
 
-# CREAT WORDPRESS DATABASE #
-echo "CREATE DATABASE $DB_DATABASE;" | mysql
+# # CREAT WORDPRESS DATABASE #
+# echo "CREATE DATABASE $DB_DATABASE;" | mysql
 
-kill $(cat /var/run/mysqld/mysqld.pid)
+# kill $(cat /var/run/mysqld/mysqld.pid)
 
-mysqld
+# mysqld
--- 42_cursus/Inception/rendu_inception/srcs/requirements/mariadb/tools/script_mysql.sh
+++ 42_cursus/Inception/rendu_inception/srcs/requirements/mariadb/tools/script_mysql.sh
@@ -1,17 +1,21 @@
 #!/bin/sh
-service mysql start 
 
-# CREATE USER #
-echo "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';" | mysql
-
-# PRIVILGES FOR ROOT AND USER FOR ALL IP ADRESS #
-echo "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';" | mysql
-echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$DB_ROOT_PASSWORD';" | mysql
-echo "FLUSH PRIVILEGES;" | mysql
-
-# CREAT WORDPRESS DATABASE #
-echo "CREATE DATABASE $DB_DATABASE;" | mysql
-
-kill $(cat /var/run/mysqld/mysqld.pid)
-
-mysqld
+# service mysql start
+mysqld_safe &
+	#	starts mysql
+sleep 5
+	# allow mysql to start
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${DB_DATABASE}\`;"
+	#	creates a table if it does not exists with ${DB_DATABEASE} name indicated in .env
+mysql -e "CREATE USER IF NOT EXISTS \`${DB_USER}\`@'localhost' IDENTIFIED BY '${DB_PASSWORD}';"
+	#	creates user if not existing already with specified password from .env
+mysql -e "GRANT ALL PRIVILEGES ON \`${DB_DATABASE}\`.* TO \`${DB_USER}\`@'%' IDENTIFIED BY '{DB_PASSWORD}';"
+	#	grants all privileges on database to the created user
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';"
+	#	here changes the root password to ${DB_ROOT_PASSWORD} from .env
+mysql -e "FLUSH PRIVILEGES"
+	#	update the last changes in mysql
+mysqladmin -u root -p${DB_ROOT_PASSWORD} shutdown
+	#	reboot mysql
+exec mysqld_safe
+	#	command that mysql ask at start
