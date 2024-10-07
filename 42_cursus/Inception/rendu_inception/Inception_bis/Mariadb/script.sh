58018944823a0b268f726b33caa5cb1726f669d4
Mateo GIRARDOT <mgirardo@made-f0Dr11s7.clusters.42paris.fr>
2024-10-07 18:02:38 +0200

    Save Commit

--- 42_cursus/Inception/rendu_inception/Inception_bis/Mariadb/script.sh
+++ 42_cursus/Inception/rendu_inception/Inception_bis/Mariadb/script.sh
@@ -3,15 +3,15 @@
 service mysql start
 
 # Create user
-echo "CREATE USER '$BDD_USER'@'%' IDENTIFIED BY '$BDD_USER_PASSWORD';" | mysql
+echo "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';" | mysql
 
 # Grant privileges for root and user for all IP address
-echo "GRANT ALL PRIVILEGES ON *.* TO '$BDD_USER'@'%' IDENTIFIED BY '$BDD_USER_PASSWORD';" | mysql
-echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$BDD_ROOT_PASSWORD';" | mysql
+echo "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';" | mysql
+echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$DB_ROOT_PASSWORD';" | mysql
 echo "FLUSH PRIVILEGES;" | mysql
 
 # Create wordpress database
-echo "CREATE DATABASE $BDD_NAME;" | mysql
+echo "CREATE DATABASE $DB_DATABASE;" | mysql
 
 kill $(cat /var/run/mysqld/mysqld.pid)
 
