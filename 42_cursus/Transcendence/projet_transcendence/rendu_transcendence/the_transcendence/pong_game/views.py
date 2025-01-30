2dad248e882380abfd842bd73d5c633ceab26dc0
Mateo GIRARDOT <mgirardo@paul-f5Ar5s1.clusters.42paris.fr>
2025-01-30 17:00:00 +0100

    Save Commit

---
+++ 42_cursus/Transcendence/projet_transcendence/rendu_transcendence/the_transcendence/pong_game/views.py
@@ -0,0 +1,16 @@
+# from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
+# Create your views here.
+
+def	pong_game(request):
+	return HttpResponse("Hello, this will be the pong game.")
+
+class ProtectedView(APIView):
+	permission_classes = [IsAuthenticated]
+
+	def get(self,request):
+		return Response({"message": "This view is protected"})
---
+++ 42_cursus/Transcendence/projet_transcendence/rendu_transcendence/the_transcendence/users/models.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+# Create your models here.
+
+class CustomUser(AbstractUser):
+	email = models.EmailField(unique=True)
+	# add any additionnal fields here
+
+	USERNAME_FIELD = 'email'
+	REQUIRED_FIELDS = ['username']
+
+	def __str__(self):
+		return self.email
