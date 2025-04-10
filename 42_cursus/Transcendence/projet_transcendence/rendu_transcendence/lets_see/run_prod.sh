52cd9c0aa17f09ae1a480bbfbaec6234a55242fa
Mateo GIRARDOT <mgirardo@made-f0Cr3s4.clusters.42paris.fr>
2025-04-10 15:02:57 +0200

    Save Commit

--- 42_cursus/Transcendence/projet_transcendence/rendu_transcendence/lets_see/run_prod.sh
+++ 42_cursus/Transcendence/projet_transcendence/rendu_transcendence/lets_see/run_prod.sh
@@ -1,23 +1,18 @@
 #!/bin/sh
 
-# cd djangotutorial
+python3 manage.py makemigrations users
+python3 manage.py migrate
 
-# ls -la
+# echo "from users.models import CustomUser; import os; user=os.getenv('DJANGO_SUPERUSER'); email=os.getenv('DJANGO_SUPERUSER_EMAIL'); password=os.getenv('DJANGO_SUPERUSER_PASS'); if not CustomUser.objects.filter(username=user).exists(): CustomUser.objects.create_superuser(username,email,password)" | python3 manage.py shell
+echo "from users.models import CustomUser; import os; CustomUser.objects.create_superuser(os.getenv('DJANGO_SUPERUSER'), os.getenv('DJANGO_SUPERUSER_EMAIL'), os.getenv('DJANGO_SUPERUSER_PASS'))" | python3 manage.py shell
 
+python3 manage.py makemigrations users
+python3 manage.py makemigrations tournament
+python3 manage.py makemigrations
+python3 manage.py migrate
 
-python manage.py makemigrations users
-python manage.py migrate
+# echo 'yes' | python3 manage.py collectstatic
 
-# echo "from users.models import CustomUser; import os; user=os.getenv('DJANGO_SUPERUSER'); email=os.getenv('DJANGO_SUPERUSER_EMAIL'); password=os.getenv('DJANGO_SUPERUSER_PASS'); if not CustomUser.objects.filter(username=user).exists(): CustomUser.objects.create_superuser(username,email,password)" | python manage.py shell
-echo "from users.models import CustomUser; import os; CustomUser.objects.create_superuser(os.getenv('DJANGO_SUPERUSER'), os.getenv('DJANGO_SUPERUSER_EMAIL'), os.getenv('DJANGO_SUPERUSER_PASS'))" | python manage.py shell
-
-python manage.py makemigrations users
-python manage.py makemigrations tournament
-python manage.py makemigrations
-python manage.py migrate
-
-# python manage.py runserver 0.0.0.0:8000
 daphne -b 0.0.0.0 -p 8000 lets_see.asgi:application
-# python manage.py test
 
 
